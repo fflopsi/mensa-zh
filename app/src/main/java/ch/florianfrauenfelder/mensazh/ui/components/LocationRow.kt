51264eca74a2651a70f59110a0f4f799e9a5ad2c
fflopsi <florian.l.frauenfelder@gmail.com>
2025-07-13 22:39:49 +0200

    Adjust location title padding

--- app/src/main/java/ch/florianfrauenfelder/mensazh/ui/components/LocationRow.kt
+++ app/src/main/java/ch/florianfrauenfelder/mensazh/ui/components/LocationRow.kt
@@ -1,16 +1,11 @@
 package ch.florianfrauenfelder.mensazh.ui.components
 
 import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.expandIn
-import androidx.compose.animation.expandVertically
-import androidx.compose.animation.shrinkOut
-import androidx.compose.animation.shrinkVertically
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import ch.florianfrauenfelder.mensazh.models.Location
@@ -28,7 +23,12 @@ fun LocationRow(
   Column(modifier = modifier.fillMaxWidth()) {
     Text(
       text = location.title,
-      modifier = Modifier.padding(8.dp),
+      modifier = Modifier.padding(
+        start = 16.dp,
+        end = 8.dp,
+        top = 16.dp,
+        bottom = 4.dp,
+      ),
     )
     Column(modifier = Modifier.fillMaxWidth()) {
       location.mensas.forEach {
