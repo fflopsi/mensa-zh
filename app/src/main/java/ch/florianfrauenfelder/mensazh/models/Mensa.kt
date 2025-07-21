44331b273e7f4edac909fd2229d52d9bb04953f6
fflopsi <florian.l.frauenfelder@gmail.com>
2025-07-21 15:29:25 +0200

    Remove unused code

--- app/src/main/java/ch/florianfrauenfelder/mensazh/models/Mensa.kt
+++ app/src/main/java/ch/florianfrauenfelder/mensazh/models/Mensa.kt
@@ -1,52 +1,30 @@
 package ch.florianfrauenfelder.mensazh.models
 
-import android.os.Parcelable
-import android.util.Log
-import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
-import androidx.compose.runtime.snapshots.SnapshotStateList
-import kotlinx.parcelize.IgnoredOnParcel
-import kotlinx.parcelize.Parcelize
-import kotlinx.parcelize.RawValue
 import java.net.URI
 import java.util.UUID
 
-@Parcelize
-//@Serializable
 data class Mensa(
-//  @Serializable(with = UUIDSerializer::class)
   val id: UUID,
   val title: String,
   val mealTime: String,
-//  @Serializable(with = URISerializer::class)
   val url: URI,
   val imagePath: String? = null,
-  private var _menus: @RawValue SnapshotStateList<Menu> = mutableStateListOf(),
-  private val initialState: State = State.Initial,
-) : Parcelable {
+) {
   enum class State { Initial, Closed, Available, Expanded }
 
+  private var _menus = mutableStateListOf<Menu>()
   var menus: List<Menu>
     get() = _menus
-    set(value) = synchronized(this){
+    set(value) = synchronized(this) {
       _menus.clear()
       _menus.addAll(value.onEach { it.mensa = this })
-      Log.d("Mensa", title)
     }
 
-  init {
-    // Trigger menus setter logic
-    menus = _menus
-  }
-
-//  @IgnoredOnParcel
-//  var location: Location? = null
-
-  @IgnoredOnParcel
-  var state by mutableStateOf(initialState)
+  var state by mutableStateOf(State.Initial)
 
   override fun toString() = title + menus
 
