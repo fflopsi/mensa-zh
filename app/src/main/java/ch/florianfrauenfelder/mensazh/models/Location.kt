9d88fbffa01afc2dac0dbf8e2d36de13fdf6f6db
fflopsi <florian.l.frauenfelder@gmail.com>
2025-07-21 15:29:25 +0200

    Remove unused code

--- app/src/main/java/ch/florianfrauenfelder/mensazh/models/Location.kt
+++ app/src/main/java/ch/florianfrauenfelder/mensazh/models/Location.kt
@@ -1,18 +1,9 @@
 package ch.florianfrauenfelder.mensazh.models
 
-import android.os.Parcelable
-import kotlinx.parcelize.Parcelize
-
-//@Parcelize
-//@Serializable
 data class Location(
   val title: String,
   val mensas: List<Mensa>,
 ) {
-//  init {
-//    mensas.forEach { it.location = this }
-//  }
-
   override fun toString() = title
 
   companion object {
