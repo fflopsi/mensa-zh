79681f0d452f2d4f99c577f5b6657341bad219e1
fflopsi <florian.l.frauenfelder@gmail.com>
2025-07-29 00:17:26 +0200

    Unify providers code

--- app/src/main/java/ch/florianfrauenfelder/mensazh/services/providers/UZHMensaProvider.kt
+++ app/src/main/java/ch/florianfrauenfelder/mensazh/services/providers/UZHMensaProvider.kt
@@ -21,50 +21,45 @@ class UZHMensaProvider(
   private val cacheService: CacheService,
   private val assetService: AssetService,
 ) : MensaProvider(cacheService) {
-  private val mensaMap: MutableMap<Mensa, UzhMensa> = HashMap()
+  override val cacheProviderPrefix = "uzh_zfv"
 
-  suspend fun getMenus(language: Language, date: Date, ignoreCache: Boolean): List<Mensa> = try {
-    val json = loadFromApi(ignoreCache, date) ?: return emptyList()
-    val menuPerMensa = parseApiRoot(
-      SerializationService.deserialize<ApiRoot>(json), mensaMap.values.toList(), language
-    )
+  private val uzhMensas = mutableListOf<UzhMensa>()
 
-    val updateMensas = mutableListOf<Mensa>()
-    menuPerMensa.entries.forEach { uzhMensa ->
-      mensaMap.entries.find { it.value == uzhMensa.key }?.key?.run {
-        menus = uzhMensa.value
-        updateMensas.add(this)
-      } ?: return@forEach
-    }
-
-    updateMensas
-  } catch (_: Exception) {
-    emptyList()
-  }
-
-
-  override fun getLocations(): List<Location> {
+  override suspend fun getLocations(): List<Location> {
     val json: String = assetService.readStringFile("uzh/locations_zfv.json") ?: return emptyList()
     return SerializationService.deserializeList<UzhLocation>(json).map { uzhLocation ->
       Location(
         title = uzhLocation.title,
         mensas = uzhLocation.mensas.map {
-          Mensa(
-            id = UUID.fromString(it.id),
-            title = it.title,
-            mealTime = it.mealTime,
-            url = URI("https://www.mensa.uzh.ch/de/menueplaene/${it.infoUrlSlug}.html"),
-          ).apply { mensaMap[this] = it }
+          uzhMensas += it
+          it.toMensa()
         },
       )
     }
+  }
+
+  suspend fun getMenus(date: Date, language: Language, ignoreCache: Boolean): List<Mensa> {
+    val mensas = mutableListOf<Mensa>()
+    try {
+      val json = loadFromApi(ignoreCache, date) ?: return emptyList()
+      val menuPerMensa = parseApiRoot(SerializationService.deserialize<ApiRoot>(json), uzhMensas, language)
+      uzhMensas.forEach { uzhMensa ->
+        mensas += uzhMensa.toMensa().apply {
+          menus = menuPerMensa[uzhMensa].orEmpty()
+        }
+      }
+    } catch (e: Exception) {
+      e.printStackTrace()
+    }
+
+    return mensas
   }
 
   private suspend fun loadFromApi(
     ignoreCache: Boolean,
     date: Date,
   ): String? {
-    val cacheKey = CACHE_PROVIDER_PREFIX + getDateTimeString(date)
+    val cacheKey = cacheProviderPrefix + getDateTimeString(date)
 
     if (!ignoreCache) {
       cacheService.readString(cacheKey)?.let { return it }
@@ -72,7 +67,6 @@ class UZHMensaProvider(
 
     val location = loadLocationFromApi(date)
     location?.let { cacheService.saveString(cacheKey, it) }
-//    Log.d("Location", location.toString())
     return location
   }
 
@@ -86,13 +80,9 @@ class UZHMensaProvider(
         setRequestProperty("Content-Type", "application/json")
         // API key included directly here to enable builds in fdroid
         // in any case, the API keys would have also been found in the APK
-        setRequestProperty(
-          "api-key",
-          "Y203MHVwaXU0OWFyeXM2MHRscWUyZncwcTpTQTJRRy83eXE5NmEzczNyRS91TjhBaysrYWl4aCs5SGhRUE9xOTk3ZzdDa1ZpdFVvQkJhK3hHN0Yyd1lLaTNu"
-        )
-        val isoDateString = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'").format(date);
-        val requestBody =
-          "{\"query\":\"query Client { organisation(where: {id: \\\"cm1tjaby3002o72q4lhhak996\\\", tenantId: \\\"zfv\\\"}) { outlets(take: 100) { slug calendar { day(date: \\\"$isoDateString\\\") { menuItems { prices { amount } ... on OutletMenuItemDish { category { name path } dish { allergens { allergen { name } } name_i18n { label locale } } } } } } } }}\",\"operationName\":\"Client\"}"
+        setRequestProperty("api-key", "Y203MHVwaXU0OWFyeXM2MHRscWUyZncwcTpTQTJRRy83eXE5NmEzczNyRS91TjhBaysrYWl4aCs5SGhRUE9xOTk3ZzdDa1ZpdFVvQkJhK3hHN0Yyd1lLaTNu")
+        val isoDateString = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'").format(date)
+        val requestBody = "{\"query\":\"query Client { organisation(where: {id: \\\"cm1tjaby3002o72q4lhhak996\\\", tenantId: \\\"zfv\\\"}) { outlets(take: 100) { slug calendar { day(date: \\\"$isoDateString\\\") { menuItems { prices { amount } ... on OutletMenuItemDish { category { name path } dish { allergens { allergen { name } } name_i18n { label locale } } } } } } } }}\",\"operationName\":\"Client\"}"
         outputStream.run {
           write(requestBody.toByteArray())
           flush()
@@ -116,31 +106,35 @@ class UZHMensaProvider(
     val result = mutableMapOf<UzhMensa, List<Menu>>()
 
     mensas.forEach { mensa ->
-      var menuItems =
-        root.data?.organisation?.outlets?.find { it.slug == mensa.slug }?.calendar?.day?.menuItems
-          ?: return@forEach
+      var menuItems = root.data?.organisation?.outlets?.find { it.slug == mensa.slug }?.calendar?.day?.menuItems
+        ?: return@forEach
 
       // filter by category if it exists and does not result in no entry
       if (mensa.categoryPath != null) {
-        menuItems =
-          menuItems.filter { item -> item.category?.path?.any { it.contains(mensa.categoryPath) } == true }
-            .toTypedArray()
+        menuItems = menuItems.filter { item ->
+          item.category?.path?.any { it.contains(mensa.categoryPath) } == true
+        }.toTypedArray()
       }
 
       val parsedMenus = mutableListOf<Menu>()
       menuItems.forEach { relevantMenu ->
         val deDescription = relevantMenu.dish?.name_i18n?.find { it.locale == "de" }?.label
-        val enDescription = relevantMenu.dish?.name_i18n?.find { it.locale == "en" }?.label
-        val descriptionRaw =
-          if (language == Language.German && deDescription != null) deDescription else enDescription
+        val descriptionRaw = if (language == Language.German && deDescription != null) {
+          deDescription
+        } else {
+          relevantMenu.dish?.name_i18n?.find { it.locale == "en" }?.label
+        }
+
         Menu(
           title = relevantMenu.category?.name ?: return@forEach,
           description = descriptionRaw?.replaceFirst(",", "\n")?.replace("\n ", "\n")
             ?: return@forEach,
-          price = relevantMenu.prices?.mapNotNull { it.amount }?.map { it.toFloat() }?.sorted()
-            ?.map { String.format(Locale.US, "%.2f", it) } ?: emptyList(),
-          allergens = relevantMenu.dish?.allergens?.mapNotNull { it.allergen?.name }
-            ?.joinToString(separator = ", "),
+          price = relevantMenu.prices?.mapNotNull { it.amount?.toFloat() }?.sorted()?.map {
+            String.format(Locale.US, "%.2f", it)
+          } ?: emptyList(),
+          allergens = relevantMenu.dish?.allergens?.mapNotNull {
+            it.allergen?.name
+          }?.joinToString(separator = ", "),
         ).run {
           if (isNoMenuNotice(this, language)) return@forEach
           parsedMenus.add(this)
@@ -158,91 +152,94 @@ class UZHMensaProvider(
     Language.German -> arrayOf("kein Abendessen", "geschlossen", "Wir sind ab Vollsemester")
   }.any { menu.description.contains(it) }
 
-  companion object {
-    const val CACHE_PROVIDER_PREFIX = "uzh_zfv"
-  }
+  @Serializable
+  private data class UzhLocation(val title: String, val mensas: List<UzhMensa>)
 
   @Serializable
-  protected class ApiRoot {
-    var data: ApiData? = null
-  }
-
-  @Serializable
-  protected class ApiData {
-    var organisation: ApiOrganisation? = null
-  }
-
-  @Serializable
-  protected class ApiOrganisation {
-    var outlets: Array<Outlet>? = null
-  }
-
-  @Serializable
-  protected class Outlet {
-    var slug: String? = null
-    var calendar: Calendar? = null
-  }
-
-  @Serializable
-  protected class Calendar {
-    var day: Day? = null
-  }
-
-  @Serializable
-  protected class Day {
-    var menuItems: Array<MenuItem>? = null
-  }
-
-  @Serializable
-  protected class MenuItem {
-    var prices: Array<Price>? = null
-    var category: Category? = null
-    var dish: Dish? = null
-  }
-
-  @Serializable
-  protected class Category {
-    var name: String? = null
-    var path: Array<String>? = null
-  }
-
-  @Serializable
-  protected class Dish {
-    var name_i18n: Array<i18nValue>? = null
-    var allergens: Array<AllergenContainer>? = null
-  }
-
-  @Serializable
-  protected class AllergenContainer {
-    var allergen: Allergen? = null
-  }
-
-  @Serializable
-  protected class Allergen {
-    var name: String? = null
-  }
-
-  @Serializable
-  protected class Price {
-    var amount: String? = null
-  }
-
-  @Serializable
-  protected class i18nValue {
-    var label: String? = null
-    var locale: String? = null
-  }
-
-  @Serializable
-  data class UzhLocation(val title: String, val mensas: List<UzhMensa>)
-
-  @Serializable
-  open class UzhMensa(
+  private data class UzhMensa(
     val id: String,
     val title: String,
     val mealTime: String,
     val infoUrlSlug: String,
     val slug: String,
     val categoryPath: String? = null,
-  )
+  ) {
+    fun toMensa() = Mensa(
+      id = UUID.fromString(id),
+      title = title,
+      mealTime = mealTime,
+      url = URI("https://www.mensa.uzh.ch/de/menueplaene/$infoUrlSlug.html"),
+    )
+  }
+
+  @Serializable
+  private class ApiRoot {
+    var data: ApiData? = null
+  }
+
+  @Serializable
+  private class ApiData {
+    var organisation: ApiOrganisation? = null
+  }
+
+  @Serializable
+  private class ApiOrganisation {
+    var outlets: Array<Outlet>? = null
+  }
+
+  @Serializable
+  private class Outlet {
+    var slug: String? = null
+    var calendar: Calendar? = null
+  }
+
+  @Serializable
+  private class Calendar {
+    var day: Day? = null
+  }
+
+  @Serializable
+  private class Day {
+    var menuItems: Array<MenuItem>? = null
+  }
+
+  @Serializable
+  private class MenuItem {
+    var prices: Array<Price>? = null
+    var category: Category? = null
+    var dish: Dish? = null
+  }
+
+  @Serializable
+  private class Category {
+    var name: String? = null
+    var path: Array<String>? = null
+  }
+
+  @Serializable
+  private class Dish {
+    var name_i18n: Array<i18nValue>? = null
+    var allergens: Array<AllergenContainer>? = null
+  }
+
+  @Serializable
+  private class AllergenContainer {
+    var allergen: Allergen? = null
+  }
+
+  @Serializable
+  private class Allergen {
+    var name: String? = null
+  }
+
+  @Serializable
+  private class Price {
+    var amount: String? = null
+  }
+
+  @Serializable
+  private class i18nValue {
+    var label: String? = null
+    var locale: String? = null
+  }
 }
