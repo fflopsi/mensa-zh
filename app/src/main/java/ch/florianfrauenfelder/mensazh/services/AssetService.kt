2569dec5b2db6c884ddd69bb7ed51046829282e2
fflopsi <florian.l.frauenfelder@gmail.com>
2025-07-29 00:18:15 +0200

    Make asset reading suspend

--- app/src/main/java/ch/florianfrauenfelder/mensazh/services/AssetService.kt
+++ app/src/main/java/ch/florianfrauenfelder/mensazh/services/AssetService.kt
@@ -1,13 +1,17 @@
 package ch.florianfrauenfelder.mensazh.services
 
 import android.content.res.AssetManager
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import java.io.IOException
 
 class AssetService(private val assetManager: AssetManager) {
-  fun readStringFile(fileName: String): String? = try {
-    assetManager.open(fileName).bufferedReader().use { it.readText() }
-  } catch (ex: IOException) {
-    ex.printStackTrace()
-    null
+  suspend fun readStringFile(fileName: String): String? = withContext(Dispatchers.IO) {
+    try {
+      assetManager.open(fileName).bufferedReader().use { it.readText() }
+    } catch (ex: IOException) {
+      ex.printStackTrace()
+      null
+    }
   }
 }
