24a068fc5fc991470934db03f0ae1d60ce3a6f2a
fflopsi <florian.l.frauenfelder@gmail.com>
2025-08-30 00:42:03 +0200

    Move viewmodel factory into viewmodel companion object

--- app/src/main/java/ch/florianfrauenfelder/mensazh/MainActivity.kt
+++ app/src/main/java/ch/florianfrauenfelder/mensazh/MainActivity.kt
@@ -4,39 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.runtime.LaunchedEffect
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.viewmodel.compose.viewModel
 import ch.florianfrauenfelder.mensazh.models.AppViewModel
-import ch.florianfrauenfelder.mensazh.services.AssetService
-import ch.florianfrauenfelder.mensazh.services.CacheService
-import ch.florianfrauenfelder.mensazh.services.favoriteMensasFlow
-import ch.florianfrauenfelder.mensazh.services.providers.showMenusInGermanToLanguage
-import ch.florianfrauenfelder.mensazh.services.saveShowMenusInGerman
-import ch.florianfrauenfelder.mensazh.services.showMenusInGermanFlow
 import ch.florianfrauenfelder.mensazh.ui.App
-import kotlinx.coroutines.flow.map
 
 class MainActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     enableEdgeToEdge()
     super.onCreate(savedInstanceState)
 
-    val viewModelFactory = object : ViewModelProvider.Factory {
-      override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        return AppViewModel(
-          assetService = AssetService(this@MainActivity.assets),
-          cacheService = CacheService(this@MainActivity.cacheDir),
-          favoriteMensas = this@MainActivity.favoriteMensasFlow,
-          initialLanguage = this@MainActivity.showMenusInGermanFlow.map { it.showMenusInGermanToLanguage },
-          saveLanguage = { this@MainActivity.saveShowMenusInGerman(it.showMenusInGerman) },
-        ) as T
-      }
-    }
-
     setContent {
-      val viewModel: AppViewModel = viewModel(factory = viewModelFactory)
+      val viewModel: AppViewModel by viewModels { AppViewModel.Factory }
 
       LaunchedEffect(Unit) {
         viewModel.refresh()
