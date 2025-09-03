c5adbc925c3910e7127cb283edcea22fe5f06ecd
fflopsi <florian.l.frauenfelder@gmail.com>
2025-09-03 17:18:10 +0200

    Add ids to locations

--- app/src/main/java/ch/florianfrauenfelder/mensazh/models/Location.kt
+++ app/src/main/java/ch/florianfrauenfelder/mensazh/models/Location.kt
@@ -1,6 +1,9 @@
 package ch.florianfrauenfelder.mensazh.models
 
+import java.util.UUID
+
 data class Location(
+  val id: UUID,
   val title: String,
   val mensas: List<Mensa>,
 ) {
@@ -8,6 +11,7 @@ data class Location(
 
   companion object {
     val dummy = Location(
+      id = UUID.randomUUID(),
       title = "Zentrum",
       mensas = List(3) { Mensa.dummy },
     )
