a6d0394840090df2fe3af82dc419eaa08c234fb2
fflopsi <florian.l.frauenfelder@gmail.com>
2025-11-11 20:23:25 +0100

    Fix system bar theming

--- app/src/main/java/ch/florianfrauenfelder/mensazh/MainActivity.kt
+++ app/src/main/java/ch/florianfrauenfelder/mensazh/MainActivity.kt
@@ -1,23 +1,49 @@
 package ch.florianfrauenfelder.mensazh
 
+import android.content.res.Configuration
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.compose.runtime.LaunchedEffect
+import androidx.lifecycle.lifecycleScope
 import androidx.room.Room
 import ch.florianfrauenfelder.mensazh.models.AppViewModel
 import ch.florianfrauenfelder.mensazh.services.CacheDatabase
+import ch.florianfrauenfelder.mensazh.services.themeFlow
 import ch.florianfrauenfelder.mensazh.ui.App
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import kotlin.time.Duration.Companion.days
 
 class MainActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
-    enableEdgeToEdge()
     super.onCreate(savedInstanceState)
+
+    lifecycleScope.launch {
+      themeFlow.collect {
+        val dark = when (it) {
+          1 -> false
+          2 -> true
+          else -> (resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK) ==
+            Configuration.UI_MODE_NIGHT_YES
+        }
+        enableEdgeToEdge(
+          statusBarStyle = SystemBarStyle.auto(
+            Color.TRANSPARENT,
+            Color.TRANSPARENT
+          ) { dark },
+          navigationBarStyle = SystemBarStyle.auto(
+            Color.TRANSPARENT,
+            Color.TRANSPARENT
+          ) { dark }
+        )
+      }
+    }
 
     val db = Room
       .databaseBuilder(applicationContext, CacheDatabase::class.java, "cache-database")
--- app/src/main/java/ch/florianfrauenfelder/mensazh/ui/theme/Theme.kt
+++ app/src/main/java/ch/florianfrauenfelder/mensazh/ui/theme/Theme.kt
@@ -1,16 +1,11 @@
 package ch.florianfrauenfelder.mensazh.ui.theme
 
-import android.graphics.Color
 import android.os.Build
-import androidx.activity.ComponentActivity
-import androidx.activity.SystemBarStyle
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
 import androidx.compose.ui.platform.LocalContext
 
 @Composable
@@ -24,24 +19,8 @@ fun MensaZHTheme(
       val context = LocalContext.current
       if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
     }
-
     darkTheme -> darkScheme
     else -> lightScheme
-  }
-
-  val context = LocalContext.current
-  DisposableEffect(darkTheme) {
-    (context as ComponentActivity).enableEdgeToEdge(
-      statusBarStyle = SystemBarStyle.auto(
-        Color.TRANSPARENT,
-        Color.TRANSPARENT,
-      ) { darkTheme },
-      navigationBarStyle = SystemBarStyle.auto(
-        Color.TRANSPARENT,
-        Color.TRANSPARENT,
-      ) { darkTheme },
-    )
-    onDispose { }
   }
 
   MaterialTheme(
