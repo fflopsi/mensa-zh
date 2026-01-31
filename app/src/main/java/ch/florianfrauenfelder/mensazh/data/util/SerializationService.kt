06bc147ea905f2519480484b7fa3dd68d048fcd4
fflopsi <florian.l.frauenfelder@gmail.com>
2026-01-31 23:15:35 +0100

    Use explicit Any supertype

--- app/src/main/java/ch/florianfrauenfelder/mensazh/data/util/SerializationService.kt
+++ app/src/main/java/ch/florianfrauenfelder/mensazh/data/util/SerializationService.kt
@@ -4,6 +4,6 @@ import kotlinx.serialization.json.Json
 
 object SerializationService {
   val safeJson = Json { ignoreUnknownKeys = true }
-  inline fun <reified T> deserializeList(json: String): List<T> = safeJson.decodeFromString(json)
+  inline fun <reified T : Any> deserializeList(json: String): List<T> = safeJson.decodeFromString(json)
   inline fun <reified T : Any> serialize(request: T) = Json.encodeToString(request)
 }
