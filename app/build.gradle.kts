90f160b25087587540bd38d958d3f362f0070eb8
fflopsi <florian.l.frauenfelder@gmail.com>
2026-04-21 00:10:10 +0200

    Update version and changelogs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
     applicationId = "ch.famoser.mensa"
     minSdk = 26
     targetSdk = 37
-    versionCode = 60
-    versionName = "2.1.0"
+    versionCode = 70
+    versionName = "2.2.0"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
