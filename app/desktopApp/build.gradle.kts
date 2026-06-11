65794156e77095aea44235261fbfaaac44038f94
fflopsi <florian.l.frauenfelder@gmail.com>
2026-06-12 01:11:57 +0200

    Add desktop app

--- app/desktopApp/build.gradle.kts
+++ app/desktopApp/build.gradle.kts
@@ -1,28 +1,33 @@
 import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 
 plugins {
-    alias(libs.plugins.kotlinJvm)
-    alias(libs.plugins.composeMultiplatform)
-    alias(libs.plugins.composeCompiler)
+  alias(libs.plugins.kotlinJvm)
+  alias(libs.plugins.composeMultiplatform)
+  alias(libs.plugins.composeCompiler)
 }
 
 dependencies {
-    implementation(projects.app.shared)
+  implementation(projects.app.shared)
 
-    implementation(compose.desktop.currentOs)
-    implementation(libs.kotlinx.coroutinesSwing)
+  implementation(compose.desktop.currentOs)
+  implementation(libs.kotlinx.coroutinesSwing)
 
-    implementation(libs.compose.uiToolingPreview)
+  implementation(libs.androidx.lifecycle.viewmodelCompose)
+  implementation(libs.androidx.datastore)
+  implementation(libs.androidx.room.runtime)
+
+  implementation(libs.compose.uiToolingPreview)
 }
 
 compose.desktop {
-    application {
-        mainClass = "ch.florianfrauenfelder.mensazh.MainKt"
+  application {
+    mainClass = "ch.florianfrauenfelder.mensazh.MainKt"
 
-        nativeDistributions {
-            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
-            packageName = "ch.florianfrauenfelder.mensazh"
-            packageVersion = "1.0.0"
-        }
+    nativeDistributions {
+      targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb, TargetFormat.Rpm, TargetFormat.AppImage)
+      packageName = "MensaZH"
+      packageVersion = "2.2.1"
+      modules("jdk.unsupported", "jdk.unsupported.desktop") // Necessary for room in compiled app
     }
+  }
 }
--- app/desktopApp/src/main/kotlin/ch/florianfrauenfelder/mensazh/main.kt
+++ app/desktopApp/src/main/kotlin/ch/florianfrauenfelder/mensazh/main.kt
@@ -1,13 +1,28 @@
 package ch.florianfrauenfelder.mensazh
 
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.viewmodel.compose.viewModel
+import ch.florianfrauenfelder.mensazh.data.local.datastore.createDataStore
+import ch.florianfrauenfelder.mensazh.data.local.room.getDatabaseBuilder
+import ch.florianfrauenfelder.mensazh.data.local.room.getRoomDatabase
+import ch.florianfrauenfelder.mensazh.ui.AppViewModel
+import ch.florianfrauenfelder.mensazh.ui.MensaApp
 
-fun main() = application {
+fun main() {
+  val container = AppContainer(createDataStore(), getRoomDatabase(getDatabaseBuilder()))
+
+  application {
     Window(
-        onCloseRequest = ::exitApplication,
-        title = "MensaZh",
+      onCloseRequest = ::exitApplication,
+      title = "MensaZH",
     ) {
-        App()
+      val appViewModel: AppViewModel = viewModel(factory = AppViewModel.Factory(container))
+      val theme by appViewModel.themeSettings.collectAsStateWithLifecycle()
+
+      MensaApp(container = container, theme = theme)
     }
+  }
 }
